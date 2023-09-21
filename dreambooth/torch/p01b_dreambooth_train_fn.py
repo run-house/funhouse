6c758e72856811ec437da893cd68d4c5894b1806
dongreenberg <dongreenberg2@gmail.com>
2023-09-21 16:09:41 -0400

    Update Stable Diffusion and add text embedding

--- dreambooth/torch/p01b_dreambooth_train_fn.py
+++ dreambooth/torch/p01b_dreambooth_train_fn.py
@@ -2,29 +2,33 @@ from pathlib import Path
 
 import runhouse as rh
 
+# This script shows a different style of specifying training, where you can turn existing functions in a git repo
+# into your remote functions. This is useful if you're working with existing codebases, and don't want wrap your
+# code in CLI boilerplate just to run it remotely.
+
 # Based on https://github.com/huggingface/diffusers/blob/main/examples/dreambooth/train_dreambooth.py
 
-def train_dreambooth(input_images_dir, class_name='person'):
+if __name__ == "__main__":
     gpu = rh.cluster(name='rh-a10x') if rh.exists('rh-a10x') else rh.cluster(name='rh-a10x', instance_type='A100:1')
-    training_function_gpu = rh.function(
-        fn='https://github.com/huggingface/diffusers/blob/v0.11.1/examples/dreambooth/train_dreambooth.py:main',
-        system=gpu,
-        env=['pip:./diffusers', 'datasets', 'accelerate', 'transformers',
-              'torch', 'torchvision'],
-        name='train_dreambooth')
+    # Need about 20 photos of the subject, and the closer they can be to 512x512 the better
+    input_images_dir = str(Path.home() / 'dreambooth/images')
+    class_name = 'person'  # Update this to be a descriptor of the subject of your photos
+
+    script_url = 'https://github.com/huggingface/diffusers/blob/v0.21.2/examples/dreambooth/train_dreambooth.py'
+    reqs = ['pip:./diffusers', 'torch==2.0.1', 'torchvision==0.15.2', 'scipy==1.11.2',
+            'datasets==2.14.5', 'accelerate==0.23.0', 'transformers==4.33.2', 'bitsandbytes==0.41.1']
+    training_function_gpu = rh.function(f'{script_url}:main', name='train_dreambooth').to(gpu, env=reqs)
     gpu.run_python(['import torch; torch.backends.cuda.matmul.allow_tf32 = True; '
                     'torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True'])
 
     remote_image_dir = 'dreambooth/instance_images'
     rh.folder(path=input_images_dir).to(system=gpu, path=remote_image_dir)
 
-    create_train_args = rh.function(
-        fn='https://github.com/huggingface/diffusers/blob/v0.11.1/examples/dreambooth/train_dreambooth.py:parse_args',
-        system=gpu, env=[])
+    create_train_args = rh.function(f'{script_url}:parse_args').to(gpu, env=[])
     train_args = create_train_args(input_args=['--pretrained_model_name_or_path', 'stabilityai/stable-diffusion-2-base',
                                                '--instance_data_dir', remote_image_dir,
                                                '--instance_prompt', f'a photo of sks {class_name}'])
-    # train_args.train_text_encoder = True
+    # train_args.train_text_encoder = True  # For higher-memory GPUs, like an A100
     train_args.class_data_dir = 'dreambooth/class_images'
     train_args.output_dir = 'dreambooth/output'
     train_args.mixed_precision = 'bf16'
@@ -42,8 +46,3 @@ def train_dreambooth(input_images_dir, class_name='person'):
     train_args.max_train_steps = 800
 
     training_function_gpu(train_args)
-
-
-if __name__ == "__main__":
-    # Need about 20 photos of the subject, and the closer they can be to 512x512 the better
-    train_dreambooth(input_images_dir=str(Path.home() / 'dreambooth/images'), class_name='person')
